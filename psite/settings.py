3e58d0afc7a285b4e80b49bfa9a4cf3dc51cda4a
MatanDarwish <55541699+MatanDarwish@users.noreply.github.com>
2020-02-08 16:56:09 +0200

    Update settings.py

--- psite/settings.py
+++ psite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '662a25x033@^dlhe)c=y6*@o37pj-2w$9a04h_n$!^)ths+f=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.75.1']
 
